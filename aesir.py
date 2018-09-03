bdc75b95a1389c7d6d810a9e9b5b309b56551655
muratdemirtas <muratdemirtastr@gmail.com>
2018-09-03 22:13:08 +0300

    screenshot alma ve clipboarda kaydetme örnekleri import edildi.

--- aesir.py
+++ aesir.py
@@ -23,6 +23,38 @@ print('Argument List:', str(sys.argv))
 
 sysloghandler.writesyslogFile( str(sys.argv))
 
+
+
+
+
+from gi.repository import Gdk
+
+win = Gdk.get_default_root_window()
+h = win.get_height()
+w = win.get_width()
+
+print ("The size of the window is %d x %d" % (w, h))
+
+pb = Gdk.pixbuf_get_from_window(win, 0, 0, w, h)
+
+if (pb != None):
+    pb.savev("screenshot.png","png", (), ())
+    print("Screenshot saved to screenshot.png.")
+else:
+    print("Unable to get the screenshot.")
+
+
+
+
+clipboard = gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+clipboard.set_image(pb)
+clipboard.store()
+
+
+
+
+
+
 if __name__ == "__main__":
     indicator = AppIndicatorExample(1)
 
